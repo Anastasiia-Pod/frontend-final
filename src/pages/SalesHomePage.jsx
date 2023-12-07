a452952fb59dbdc95fcf773c33ae6f7258220cb1
Anastasiia Podopryhora <anastasia95podoprigora@gmail.com>
2023-12-07 20:38:38 +0100

    SalesHomePage draft

--- src/pages/SalesHomePage.jsx
+++ src/pages/SalesHomePage.jsx
@@ -3,17 +3,15 @@ import { useContext } from "react";
 import { Link } from 'react-router-dom';
 import { AuthContext } from "../context/auth.context";
 
-function LogHomePage() {
+function SalesHomePage() {
   const {user} = useContext(AuthContext); 
   return (
     <div className="homePage">
       <h1 className="navButton">Hello, {user.name}! This is your profile page!</h1>
       <Link to="/orders"> <button className="navButton">View all Orders</button> </Link>
-      <Link to="/logistics/partners"> <button className="navButton">View all Partners</button> </Link>
-      <Link to="/logistics/partners/new"> <button className="navButton">Create new Partner</button> </Link>
-      <Link to="/logistics/routes"> <button className="navButton">Generate a route</button> </Link>
+      <Link to="/sales/orders/new"> <button className="navButton">Create new Order</button> </Link>
     </div>
   );
 }
 
-export default LogHomePage;
+export default SalesHomePage;
