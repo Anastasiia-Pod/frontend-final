6c35a03e6c926d1c02677eb98cb8ddaa87018817
Anastasiia Podopryhora <anastasia95podoprigora@gmail.com>
2023-12-07 20:30:34 +0100

    LogHomePage draft

--- src/pages/LogHomePage.jsx
+++ src/pages/LogHomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import HomeImage from '/images/HomePage.gif';
+import { useContext } from "react";  
+import { Link } from 'react-router-dom';
+import { AuthContext } from "../context/auth.context";
 
 function LogHomePage() {
+  const {user} = useContext(AuthContext); 
   return (
     <div className="homePage">
-      <h1 className="homePageComp">SmileMile Logistics</h1>
-      <img src={HomeImage} alt="Description of the image" />
+      <h1 className="navButton">Hello, {user.name}! This is your profile page!</h1>
+      <Link to="/logistics/orders"> <button className="navButton">View all Orders</button> </Link>
+      <Link to="/logistics/partners"> <button className="navButton">View all Partners</button> </Link>
+      <Link to="/logistics/partners/new"> <button className="navButton">Create new Partner</button> </Link>
+      <Link to="/logistics/routes"> <button className="navButton">Generate a route</button> </Link>
     </div>
   );
 }
